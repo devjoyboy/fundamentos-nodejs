51d3547417072cb46f1418b1092a2c00e5bf1fd3
devjoyboy <devjoyboy@gmail.com>
2022-04-30 10:34:03 -0300

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -1,13 +1,58 @@
+const { request } = require('express')
+const { response } = require('express')
 const express = require('express')
 
 const app = express()
 
-// localhost:3333
+/**
+ * GET - Buscar uma informação dentro do servidor.
+ * POST - Inserir uma informação no servidor.
+ * PUT - Alterar uma informação no servidor.
+ * PATCH - ALterar uma informação específica no servidor.
+ * DELETE - Deletar uma informação no servidor.
+  */
 
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Hello World Ignite!"
-    })
+app.get("/courses", (request, response) => {
+    return response.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3"
+    ])
+})
+
+app.post("/courses", (request, response) => {
+    return response.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 3",
+        "Curso 4"
+    ])
+})
+
+app.put("/courses/:id", (request, response) => {
+    return response.json([
+        "Curso 6",
+        "Curso 2",
+        "Curso 3",
+        "Curso 4"
+    ])
+})
+
+app.patch("/courses/:id", (request, response) => {
+    return response.json([
+        "Curso 7",
+        "Curso 8",
+        "Curso 3",
+        "Curso 4"
+    ])
+})
+
+app.delete("/courses/:id", (request, response) => {
+    return response.json([
+        "Curso 1",
+        "Curso 2",
+        "Curso 4"
+    ])
 })
 
 app.listen(3333)
